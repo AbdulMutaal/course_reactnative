1648d5a65aa45c52d02e2d2ab54587674c991c62
Abdul-mutaal <abdulmutaalazhar@gmail.com>
2020-08-22 20:04:31 +0500

    Component pt 1 & 2

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponents';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hell Wrld!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#fff'
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <Main />
+    );
   }
-});
+}
